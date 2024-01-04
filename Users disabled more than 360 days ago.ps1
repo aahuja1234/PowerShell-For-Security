31a4909cb06da076684aeca6d324b3b4aaa40423
Akash Ahuja <78800132+aahuja1234@users.noreply.github.com>
2024-01-04 09:10:11 -0500

    Add files via upload

---
+++ Users disabled more than 360 days ago.ps1
@@ -0,0 +1,15 @@
+﻿# Import the Active Directory module if not already loaded
+Import-Module ActiveDirectory
+
+# Get the current date
+$today = Get-Date
+
+# Define the threshold for disabled days (360 days)
+$threshold = (Get-Date).AddDays(-360)
+
+# Get disabled users that were disabled more than 90 days ago
+$disabledUsers = Get-ADUser -Filter {Enabled -eq $false -and whenChanged -le $threshold} -Properties whenChanged
+
+# Display the results
+$disabledUsers | Select-Object Name, SamAccountName, Enabled, whenChanged | Format-Table -AutoSize
+$disabledUsers | Select-Object Name, SamAccountName, Enabled, whenChanged | Export-Csv -Path "C:\DisabledMoreThan360DaysAgo.csv" -NoTypeInformation
